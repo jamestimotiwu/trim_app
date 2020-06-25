d6c9bd05d91a8e642ac34e3f9252e6ad3eb51533
James Timotiwu <17231755+jamestimotiwu@users.noreply.github.com>
2020-06-25 01:49:29 +0000

    Add dep and button

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Load from './pages/Load.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Route path="/" component={Load} />
+      </BrowserRouter>
     </div>
   );
 }
