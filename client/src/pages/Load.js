458585c02e223b90c728f8bc703a9873cd73a5ce
James Timotiwu <17231755+jamestimotiwu@users.noreply.github.com>
2020-06-25 02:27:43 +0000

    Add accept video input

--- client/src/pages/Load.js
+++ client/src/pages/Load.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
-import { Typography, Button } from '@material-ui/core';
+import { Typography, Button, Input} from '@material-ui/core';
+import './Load.css';
 
 class Load extends Component {
   constructor(props) {
     super(props);   
+
+    this.state = {
+      videos: [],
+      status: 'pending'
+    };
+
+    this.handleLoadVideo = this.handleLoadVideo.bind(this);
+  }
+
+  handleLoadVideo(event) {
+    const videos = []
+
+    for (const file of event.target.files) {
+      videos.push([file, URL.createObjectURL(file)]);
+    }
+
+    this.setState(state => ({
+      ...state,
+      videos
+    }));
+    console.log(this.state.videos)
+    
   }
 
   render() {
     return (
       <div>
-        <Button variant="contained" color="secondary">
-          Load video
-        </Button>
+        <label className="uploader-wrapper" htmlFor="upload-video">
+          <Button variant="contained" 
+                component="span"
+                color="secondary"
+                disabled={this.state.status === 'loaded'}
+                >
+            Load video
+          </Button>
+        </label>
+        <input
+          accept="video/*"
+          style={{display: "None"}}
+          id="upload-video"
+          type="file"
+          onChange={this.handleLoadVideo}
+        />
       </div>
     )
   }
