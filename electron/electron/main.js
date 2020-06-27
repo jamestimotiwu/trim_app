84be10092aa8e16a82ac28974539c3e4aa475d4b
James Timotiwu <architect.idol5@gmail.com>
2020-06-27 11:07:54 -0400

    Modify to use ffmpeg-static

--- electron/electron/main.js
+++ electron/electron/main.js
@@ -1,8 +1,16 @@
+
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs')
 const path = require('path');
 const url = require('url');
 const { channels } = require('../src/shared/constants');
+
+const { spawn } = require('child_process');
+
+const ffmpegPath = require('ffmpeg-static').replace(
+	'app.asar',
+	'app.asar.unpacked'
+);
 
 let mainWindow;
 
@@ -18,12 +26,14 @@ function createWindow () {
 	height: 800,
 	webPreferences: {
 	  preload: path.join(__dirname, 'preload.js'),
+	  nodeIntegrationInWorker: true,
 	},
   });
   mainWindow.loadURL(startUrl);
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
+
 }
 
 app.on('ready', createWindow);
@@ -51,3 +61,44 @@ ipcMain.on(channels.WRITE_VIDEO_FILE, (event, {path, file}) => {
   fs.writeFileSync(path, file);
   console.log("written to" + path);
 });
+
+ipcMain.on(channels.FFMPEG_TRIM, (event, {sliderval, duration, file}) => {
+  let from = sliderval[0]/100 * duration
+  let to = sliderval[1]/100 * duration
+  let args = [
+    '-nostdin',
+	'-hide_banner',
+	'-i',
+	file.path,
+	'-ss',
+	from,
+	'-to',
+	to,
+	'-c:v',
+	'copy',
+	'-c:a',
+	'copy',
+	file.path]
+
+  var proc = spawn(ffmpegPath, args)
+  proc.stdout.on('data', data => {
+	console.log(data);
+  })
+
+//await ffmpeg.run("-nostdin -hide_banner -i temp.mov -ss " + from + " -to " + to + " -c:v copy -c:a copy output.mov")
+  
+    //const data = ffmpeg.read('output.mov');
+    //console.log(data)
+    //const blob = URL.createObjectURL(new Blob([data.buffer],{ type: 'video/quicktime' }))
+  
+    //fs.writeFileSync(file.path, Buffer(data.buffer));
+	/*
+	fs.readFileSync(new URL(file.path))
+    console.log("written to" + file.path);
+    event.sender.send(channels.FFMPEG_TRIM, {
+      blobfile: blob,
+    });
+  })();*/
+});
+
+
