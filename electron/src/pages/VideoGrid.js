b4bb3a63a43cbd57894160e3c0b830519bd59560
James Timotiwu <architect.idol5@gmail.com>
2020-07-02 21:11:00 -0400

    Working thumbnailing videos with canvas context and loading

--- electron/src/pages/VideoGrid.js
+++ electron/src/pages/VideoGrid.js
@@ -7,9 +7,61 @@ class VideoGrid extends Component {
 
 		this.state = {
 			videos: [],
+			thumbs: [],
 		}
+		this.canvasRef = React.createRef();
+		this.thVidRef = null
 
 		this.handleLoadVideo = this.handleLoadVideo.bind(this);
+	}
+  
+	thumbVidRef = ref => {
+		var ctx = this.canvasRef.current.getContext('2d');
+		
+		if (ref != null) {
+			this.thVidRef = ref;
+			this.thVidRef.addEventListener('loadedmetadata', () => {
+			  console.log(this.thVidRef.videoHeight);
+				this.canvasRef.current.height = this.thVidRef.videoHeight/3;
+				this.canvasRef.current.width = this.thVidRef.videoWidth/3;
+				this.thVidRef.currentTime = 0.5;
+			});
+
+			this.thVidRef.addEventListener('seeked', () => {
+				ctx.drawImage(this.thVidRef, 0, 0,
+											this.canvasRef.current.width,
+											this.canvasRef.current.height);
+				this.canvasRef.current.toBlob((blob) => {
+				  console.log(blob);
+					let thumbs = this.state.thumbs;
+					const blob_link = URL.createObjectURL(blob);
+					thumbs.push(blob_link);
+					
+					this.setState({
+						thumbs,
+					});
+				}, 'image/jpeg');
+				
+			});
+		}
+	}
+
+	renderVideoThumb() {
+		if (this.state.videos.length > 0) {
+			return ( 
+				<video style={{display: "none"}} ref={this.thumbVidRef} preload="metadata" width="100%" height="auto" src={this.state.videos[this.state.videos.length - 1][0]}></video>
+			)
+		}
+	}
+
+	renderImageGrid() {
+		return this.state.thumbs.map(thumb => {
+			return (
+				<Grid item xs={4}>
+					<img width="100%" height="auto" src={thumb}/>
+				</Grid>
+			);
+		});
 	}
 
 	renderVideoGrid() {
@@ -44,6 +96,7 @@ class VideoGrid extends Component {
 		return(
       <div>
         <Grid container alignItems="center" justify="center" spacing={1}>
+					<canvas style={{display: "none"}} ref={this.canvasRef}/>
 					<Grid item xs={12}>
 						<div style={{margin:"0 auto",paddingTop: "10em"}} id="file-load">
 							<label htmlFor="upload-video">
@@ -64,7 +117,9 @@ class VideoGrid extends Component {
 							/>
 						</div>
 					</Grid>
-					{this.renderVideoGrid()}
+					{this.renderVideoThumb()}
+					{this.renderImageGrid()}
+
 				</Grid>
 			</div>
 		);
